f59c12a68abb1d7e26a7ac9b624452e4af7393e9
Arve Hjønnevåg <arve@android.com>
2018-04-11 15:33:41 -0700

    qemu: Allocate more RAM for bl31
    The old allocation does not leave enough room to enable the trusty spd.
    
    Bug: 124390919
    Change-Id: Ie26eff57e0ec111cf98e2491cf464b3a4a5bcf44

--- plat/qemu/include/platform_def.h
+++ plat/qemu/include/platform_def.h
@@ -124,7 +124,7 @@
  * Put BL2 just below BL3-1. BL2_BASE is calculated using the current BL2 debug
  * size plus a little space for growth.
  */
-#define BL2_BASE			(BL31_BASE - 0x25000)
+#define BL2_BASE			(BL31_BASE - 0x15000)
 #define BL2_LIMIT			BL31_BASE
 
 /*
@@ -133,7 +133,7 @@
  * Put BL3-1 at the top of the Trusted SRAM. BL31_BASE is calculated using the
  * current BL3-1 debug size plus a little space for growth.
  */
-#define BL31_BASE			(BL31_LIMIT - 0x20000)
+#define BL31_BASE			(BL31_LIMIT - 0x30000)
 #define BL31_LIMIT			(BL_RAM_BASE + BL_RAM_SIZE)
 #define BL31_PROGBITS_LIMIT		BL1_RW_BASE
 
